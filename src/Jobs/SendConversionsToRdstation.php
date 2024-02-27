bf0b8327ce0e852111b4b91583d3bef23a2a197d
Irineu Junior <irineu@fmd.ag>
2024-02-27 15:28:10 -0300

    feat(url) movimenta a url de envio do lead

--- src/Jobs/SendConversionsToRdstation.php
+++ src/Jobs/SendConversionsToRdstation.php
@@ -38,7 +38,7 @@ class SendConversionsToRdstation implements ShouldQueue
                 'token_rdstation' => config('laravel-rdstation.public_key'),
             ] + $this->data;
 
-        $client->request('POST', 'https://www.rdstation.com.br/api/1.2/conversions', [
+        $client->request('POST', config('laravel-rdstation.url'), [
             'form_params' => $formParams,
         ]);
     }
--- src/config/laravel-rdstation.php
+++ src/config/laravel-rdstation.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
+    'url' => env('RDSTATION_URL', 'https://www.rdstation.com.br/api/1.2/conversions'),
     'public_key' => env('RDSTATION_PUBLIC_KEY', ''),
 ];
